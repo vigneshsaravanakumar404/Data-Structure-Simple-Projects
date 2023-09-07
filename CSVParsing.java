19f483812fe1023f9f5775361456540414906e8d
vigneshsaravanakumar404 <10021913@sbstudents.ir>
2023-09-07 12:15:44 -0400

    Day One Progress
    Signed-off-by: vigneshsaravanakumar404 <10021913@sbstudents.ir>

---
+++ CSVParsing.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CSVParsing {
+    public static void main(String[] args) {
+
+        try {
+            File file = new File("dayOneCSVData.txt");
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+                System.out.println(values[0] + " is a bit odd");
+                System.out.println(values[1] + " is even tempered");
+            }
+    }
+}
