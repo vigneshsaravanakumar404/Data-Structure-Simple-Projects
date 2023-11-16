6cf3da570d2eebb41a94c9f0104bb05599a651a4
Vignesh Saravanakumar <10021913@sbstudents.org>
2023-11-16 11:29:18 -0500

    Clear System
    Signed-off-by: Vignesh Saravanakumar <10021913@sbstudents.org>

--- Queue/BookOrder/BookInventorySystem.java
+++ Queue/BookOrder/BookInventorySystem.java
@@ -2,6 +2,9 @@ public class BookInventorySystem{
 
     public static void main(String[] args) {
 
+        System.out.print("\033[H\033[2J");  
+
+        
         // Create a BookOrderProcessor
         BookOrderProcessor orderProcessor = new BookOrderProcessor();
 
