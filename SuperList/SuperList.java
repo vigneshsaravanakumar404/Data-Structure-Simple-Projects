7f10fb9c10b716df1301f62bd36ef317860a5b60
Vignesh Saravanakumar <10021913@sbstudents.org>
2023-12-08 11:03:53 -0500

    Left off on Day 1 #3
    Signed-off-by: Vignesh Saravanakumar <10021913@sbstudents.org>

--- SuperList/SuperList.java
+++ SuperList/SuperList.java
@@ -6,15 +6,35 @@ public class SuperList<T> {
     private ListNode<T> root;
     private ListNode<T> end;
     private int size;
+    private ListNode<T>[] elements;
 
-    // Ask if you need an array to keep track of the elements
 
     public SuperList(T element){
         this.element = element;
         this.root = new ListNode<T>(element);
         this.end = this.root;
         this.size = 0;
+        this.elements = new ListNode[1000]; // 1000 is arbitrary
     }
 
+    // TODO: Check to make sure it is in size
+    public void add(T element){
+        elements[size] = new ListNode<T>(element);
+        this.size++;
+    }
+
+    public int size(){
+        return this.size;
+    }
+
+    public String toString(){
+        String output = "[";
+        for (int i = 0; i < this.size; i++){
+            output += elements[i].getVal() + ", ";
+        }
+        return output + "]";
+    }
+
+    // TODO: Write unit test
     
 }
