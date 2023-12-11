43530e62497f7fba09b607cbe5069a481123a931
Vignesh Saravanakumar <10021913@sbstudents.org>
2023-12-11 16:18:11 -0500

    Last commit for 12/11/2023
    Signed-off-by: Vignesh Saravanakumar <10021913@sbstudents.org>

--- SuperList/SuperList.java
+++ SuperList/SuperList.java
@@ -1,7 +1,49 @@
 package SuperList;
 
 public class SuperList<T> {
+    public class ListNode<T> {
+    
+        private T element;
+        private ListNode<T> prev;
+        private ListNode<T> next;
+    
+        public ListNode(T element) {
+            this.element = element;
+            this.next = null;
+            this.prev = null;
+        }
+    
+        public void setPrev(ListNode<T> prev) {
+            this.prev = prev;
+        }
+    
+        public void setNext(ListNode<T> next) {
+            this.next = next;
+        }
+    
+        public ListNode<T> getNext() {
+            return this.next;
+        }
+    
+        public ListNode<T> getPrev() {
+            return this.prev;
+        }
+    
+        public T getElement() {
+            return element;
+        }
+    
+        public boolean hasNext() {
+            return this.next != null;
+        }
+    
+        public boolean hasPrev() {
+            return this.prev != null;
+        }
+    
+    }
 
+    
     private ListNode<T> root;
     private ListNode<T> end;
     private int size;
