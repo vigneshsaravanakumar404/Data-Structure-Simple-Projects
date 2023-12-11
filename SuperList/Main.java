38f9d5b54cd9d01fb51cfdf93cbd61ac7fc4bcd8
Vignesh Saravanakumar <10021913@sbstudents.org>
2023-12-11 17:08:43 -0500

    Main.java Creation
    Signed-off-by: Vignesh Saravanakumar <10021913@sbstudents.org>

--- SuperList/Main.java
+++ SuperList/Main.java
@@ -2,4 +2,17 @@ package SuperList;
 
 public class Main {
     
+    public static void main(String[] args) {
+        SuperList<Integer> superList = new SuperList<>();
+
+        // Test adding elements
+        superList.add(1);
+        superList.add(2);
+        superList.add(3);
+        superList.add(4);
+
+        // Test toString method
+        System.out.println("List contents: " + superList.toString()); // Expected: [1, 2, 3, 4]
+        System.out.println("List size: " + superList.size()); // Expected: 4
+    }
 }
