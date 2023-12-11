ed02a170f3dfbaed33dd84e50f1f870f973e6d3c
Vignesh Saravanakumar <10021913@sbstudents.org>
2023-12-11 16:14:23 -0500

    SuperList #2 Complete
    Signed-off-by: Vignesh Saravanakumar <10021913@sbstudents.org>

--- SuperList/SuperList.java
+++ SuperList/SuperList.java
@@ -2,39 +2,15 @@ package SuperList;
 
 public class SuperList<T> {
 
-    private T element;
     private ListNode<T> root;
     private ListNode<T> end;
     private int size;
-    private ListNode<T>[] elements;
 
 
-    public SuperList(T element){
-        this.element = element;
-        this.root = new ListNode<T>(element);
-        this.end = this.root;
-        this.size = 0;
-        this.elements = new ListNode[1000]; // 1000 is arbitrary
+    public SuperList() {
+        root = null;
+        end = null;
+        size = 0;
     }
-
-    // TODO: Check to make sure it is in size
-    public void add(T element){
-        elements[size] = new ListNode<T>(element);
-        this.size++;
-    }
-
-    public int size(){
-        return this.size;
-    }
-
-    public String toString(){
-        String output = "[";
-        for (int i = 0; i < this.size; i++){
-            output += elements[i].getVal() + ", ";
-        }
-        return output + "]";
-    }
-
-    // TODO: Write unit test
     
 }
