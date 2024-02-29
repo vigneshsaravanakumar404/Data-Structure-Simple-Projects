1e40ee1c5420fb956cc2734c54bef5751e2be914
Vignesh Saravanakumar <10021913@sbstudents.org>
2024-02-29 16:21:22 -0500

    Load in Dataset
    Signed-off-by: Vignesh Saravanakumar <10021913@sbstudents.org>

--- Trie/TrieRunner.java
+++ Trie/TrieRunner.java
@@ -1,24 +1,19 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class TrieRunner {
     public static void main(String[] args) {
         Trie trie = new Trie();
 
-        // Add words to the Trie
-        trie.add("then");
-        trie.add("them");
-        trie.add("there");
-        trie.add("theme");
-
-        // Test contains method
-        System.out.println("contains(\"them\") -> " + trie.contains("them")); // Should print true
-        System.out.println("contains(\"the\") -> " + trie.contains("the")); // Should print false
-        System.out.println("contains(\"this\") -> " + trie.contains("this")); // Should print false
-
-        // Test mostLikelyNextChar method
-        System.out.println("mostLikelyNextChar(\"the\") -> '" + trie.mostLikelyNextChar("the") + "'"); // Should print
-        // 'm'
-        System.out.println("mostLikelyNextChar(\"this\") -> '" + trie.mostLikelyNextChar("this") + "'"); // Should print
-        // '_'
-        System.out.println("mostLikelyNextChar(\"theme\") -> '" + trie.mostLikelyNextChar("theme") + "'"); // Should
-        // print '_'
+        // Load words from words.txt
+        try (BufferedReader br = new BufferedReader(new FileReader("words.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                trie.add(line.toLowerCase());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
